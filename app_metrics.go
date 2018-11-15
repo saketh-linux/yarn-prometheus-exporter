0210d8973d3ab37e85dba60c55475f4ae0c6f1a2
Nandgopal <rnandgop@LAMU02WJ20GHTD6.uhc.com>
2018-11-14 18:14:24 -0800

    Added additional queues

--- app_metrics.go
+++ app_metrics.go
@@ -4,10 +4,12 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -21,6 +23,7 @@ type collectorApps struct {
 	allocatedMB       *prometheus.Desc
 	allocatedVCores   *prometheus.Desc
 	runningContainers *prometheus.Desc
+	elapsedTime       *prometheus.Desc
 }
 
 const appMetricsNamespace = "yarn_apps"
@@ -34,6 +37,7 @@ func newAppsCollector(endpoint *url.URL, username string, password string, queue
 		allocatedMB:       newFuncMetric(appMetricsNamespace, "allocated_memory", "Allocated memory", []string{"user", "name", "id", "state", "applicationType", "queue"}),
 		allocatedVCores:   newFuncMetric(appMetricsNamespace, "allocatedVCores", "Allocated virtual cores", []string{"user", "name", "id", "state", "applicationType", "queue"}),
 		runningContainers: newFuncMetric(appMetricsNamespace, "runningContainers", "Running containers", []string{"user", "name", "id", "state", "applicationType", "queue"}),
+		elapsedTime:       newFuncMetric(appMetricsNamespace, "elapsedTime", "Elaspsed Time", []string{"user", "name", "id", "state", "applicationType", "queue"}),
 	}
 }
 
@@ -41,31 +45,44 @@ func (c *collectorApps) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.allocatedMB
 	ch <- c.allocatedVCores
 	ch <- c.runningContainers
+	ch <- c.elapsedTime
 }
 
 func (c *collectorApps) Collect(ch chan<- prometheus.Metric) {
-	appData, err := fetchAppsMetrics(c.endpoint, c.username, c.password, c.queueName)
-	if err != nil {
-		log.Println("Error while collecting apps metrics from YARM: " + err.Error())
-	}
 
-	appMetrics := appData["apps"]["app"]
+	queues := strings.Split(c.queueName, ",")
 
-	for _, value := range appMetrics {
+	for _, yarn_queue := range queues {
+		yarn_queue = strings.TrimSpace(yarn_queue)
 
-		allocatedMB := value["allocatedMB"].(float64)
-		allocatedVCores := value["allocatedVCores"].(float64)
-		runningContainers := value["runningContainers"].(float64)
+		fmt.Println(yarn_queue)
 
-		user := value["user"].(string)
-		name := value["name"].(string)
-		state := value["state"].(string)
-		applicationType := value["applicationType"].(string)
-		id := value["id"].(string)
+		appData, err := fetchAppsMetrics(c.endpoint, c.username, c.password, yarn_queue)
+		if err != nil {
+			log.Println("Error while collecting apps metrics from YARN: " + err.Error())
+		}
 
-		ch <- prometheus.MustNewConstMetric(c.allocatedMB, prometheus.GaugeValue, allocatedMB, user, name, id, state, applicationType, c.queueName)
-		ch <- prometheus.MustNewConstMetric(c.allocatedVCores, prometheus.GaugeValue, allocatedVCores, user, name, id, state, applicationType, c.queueName)
-		ch <- prometheus.MustNewConstMetric(c.runningContainers, prometheus.GaugeValue, runningContainers, user, name, id, state, applicationType, c.queueName)
+		appMetrics := appData["apps"]["app"]
+
+		for _, value := range appMetrics {
+
+			allocatedMB := value["allocatedMB"].(float64)
+			allocatedVCores := value["allocatedVCores"].(float64)
+			runningContainers := value["runningContainers"].(float64)
+			elapsedTime := value["elapsedTime"].(float64)
+
+			user := value["user"].(string)
+			name := value["name"].(string)
+			state := value["state"].(string)
+			applicationType := value["applicationType"].(string)
+			id := value["id"].(string)
+
+			ch <- prometheus.MustNewConstMetric(c.allocatedMB, prometheus.GaugeValue, allocatedMB, user, name, id, state, applicationType, yarn_queue)
+			ch <- prometheus.MustNewConstMetric(c.allocatedVCores, prometheus.GaugeValue, allocatedVCores, user, name, id, state, applicationType, yarn_queue)
+			ch <- prometheus.MustNewConstMetric(c.runningContainers, prometheus.GaugeValue, runningContainers, user, name, id, state, applicationType, yarn_queue)
+			ch <- prometheus.MustNewConstMetric(c.elapsedTime, prometheus.GaugeValue, elapsedTime, user, name, id, state, applicationType, yarn_queue)
+		}
+
 	}
 
 	return
@@ -101,6 +118,8 @@ func fetchAppsMetrics(u *url.URL, username string, password string, queueName st
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
+
+	// fmt.Println(string(body))
 
 	if err != nil {
 		return nil, err
